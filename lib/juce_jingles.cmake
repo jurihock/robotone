b2a649904a63b4d4cbe82ad0c79732627896a02b
Jürgen Hock <juergen.hock@jurihock.de>
2025-06-04 23:24:01 +0200

    Outsource juce jingles

---
+++ lib/juce_jingles.cmake
@@ -0,0 +1,9 @@
+# https://github.com/jurihock/juce_jingles
+
+CPMAddPackage(
+  NAME juce_jingles
+  VERSION main
+  GIT_TAG main
+  GITHUB_REPOSITORY jurihock/juce_jingles
+  DOWNLOAD_ONLY YES
+  SOURCE_DIR "${CMAKE_SOURCE_DIR}/modules/juce_jingles")
