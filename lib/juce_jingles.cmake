64ebf290f77fbe535565f852f6d43b7e2dc6e41b
Jürgen Hock <juergen.hock@jurihock.de>
2025-06-07 11:18:11 +0200

    Update juce_jingles.cmake

--- lib/juce_jingles.cmake
+++ lib/juce_jingles.cmake
@@ -5,9 +5,8 @@ CPMAddPackage(
   VERSION main
   GIT_TAG main
   GITHUB_REPOSITORY jurihock/juce_jingles
-  DOWNLOAD_ONLY YES
-  SOURCE_DIR "${CPM_SOURCE_CACHE}/juce_jingles")
+  DOWNLOAD_ONLY YES)
 
 if(juce_jingles_ADDED)
-  juce_add_module("${juce_jingles_SOURCE_DIR}")
+  juce_add_module("${juce_jingles_SOURCE_DIR}/juce_jingles")
 endif()
